18e9156173365345250e8bd43ccc403d99997dae
geekbrains <volkn139@gmail.com>
2021-01-05 13:17:45 +0300

    Initial commit

---
+++ src/lisson/gb/Frame.java
@@ -0,0 +1,20 @@
+package lisson.gb;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+    Frame(){
+        setTitle("Чат");
+        setSize(new Dimension(400,400));
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        BigTextPanel bigTextPanel = new BigTextPanel();
+        SendPanel sendPanel = new SendPanel(bigTextPanel.getTextArea());
+        add(bigTextPanel, BorderLayout.CENTER);
+        add(sendPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+}
---
+++ src/lisson/gb/Task4.java
@@ -0,0 +1,7 @@
+package lisson.gb;
+import javax.swing.*;
+public class Task4 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Frame::new);
+    }
+}
